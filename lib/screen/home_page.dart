c17d6129c86beb80c5c135798f50ae63a33383fc
Shuvo prosad sarnakar <shuvoprosad@gmail.com>
2025-11-14 12:39:00 +0600

    launch config updated

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text('counter', style: Theme.of(context).textTheme.headlineMedium),
+          ],
+        ),
+      ),
+    );
+  }
+}
